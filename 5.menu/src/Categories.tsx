945171a2eca7bbccf375874c80808fded3c5c074
askhalsa <arshdeep.singh@crownstack.com>
2021-12-02 04:45:31 +0530

    complete menu project

---
+++ 5.menu/src/Categories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Categories = (props: { categories: any; filterItems: any; }) => {
+    const { categories, filterItems } = props
+  return (
+    <div className="btn-container">
+      {categories.map((category: string, index: number) => {
+        return (
+          <button type="button" className="filter-btn" key={index} onClick={() => filterItems(category)}>
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
